fb2e46cce203793fafb17795516c4730ba9c2ba7
Gillian Pierce <gmpierce@ualberta.ca>
2019-01-20 09:43:51 -0700

    she's coming together alright, maybe not a prize winning pig but would definitely be tasty

--- client/src/components/legenditem.js
+++ client/src/components/legenditem.js
@@ -5,11 +5,11 @@ import '../styles/legenditem.css'
 const LegendItem = ({color, caption, count}) => {
     return (
         <div className="legend-item-container">
-            <svg className="circle" height="50" width="50">
-                <circle cx="25" cy="25" r="20" stroke={color} strokeWidth="3" fill={color}></circle>
-                <text x="50%" y="50%" textAnchor="middle" stroke="white" fill="white" strokeWidth="1px" dy=".3em">{count}%</text>
+            <svg className="circle" height="80" width="80">
+                <circle cx="40" cy="40" r="30" stroke={color} strokeWidth="3" fill={color}></circle>
+                <text x="50%" y="50%" textAnchor="middle" stroke="white" fill="white" strokeWidth="0.5pxpx" dy=".3em">{count}%</text>
             </svg>
-            <div className="caption"><h5>{caption}</h5></div>
+            <div className="caption"><h3>{caption}</h3></div>
         </div>
     );
 
