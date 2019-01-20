b54de5cb9277fbc7ebe636b460787c61e90e9223
Gillian Pierce <gmpierce@ualberta.ca>
2019-01-20 02:07:20 -0700

    das one sexy ui bruthaaaaaaaa

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,8 @@ import 'typeface-vt323';
 import 'typeface-roboto-mono';
 import 'typeface-zcool-qingke-huangyou';
 import Search from './search';
+import Viz from './viz';
+import EmbedTweet from './tweet';
 
 
 const styles = theme => ({
@@ -18,16 +20,25 @@ const styles = theme => ({
 
 
 class App extends Component {
+  setTweetUrl = (tweetId) => {
+    this.setState({tweetId: tweetId});
+  }
+
+  state = {tweetId: 'no tweet id yet'};
+
   render() {
     return (
-      <div className="App">     
+      <div className="App">   
         <div className="header">
           <h1 className="app-title">spotbot</h1>
         </div>
         <div className="left"></div>
         <div className="right"></div>
         <div className="search">
-          <Search/>
+          <Search setTweetUrl={this.setTweetUrl}/>
+        </div>
+        <div className="tweet">
+          <EmbedTweet tweetId={this.state.tweetId}/>
         </div>
         <div className="viz">
           <Viz/>
--- server/server.js
+++ server/server.js
@@ -2,16 +2,35 @@ const botCheck = require('./requests/BotCheck');
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var http = require('http');
 var app = express();
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+io = socketIO(server);
 
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    var botCount = 0;
+    var humanCount = 0;
+    setInterval(() => {
+      botCount++;
+      humanCount++;
+      client.emit('timer', {'botCount': botCount, 'humanCount': humanCount});
+    }, interval);
+  });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/search',function(req,res){
-  var url=req.body.url;
-  console.log("search twitter url: "+url);
-});
-app.listen(process.env.PORT,function(){
-  console.log("Started on PORT 5000");
-})
+// app.post('/search',function(req,res){
+//   var url=req.body.url;
+//   console.log("search twitter url: "+url);
+// });
+// app.listen(process.env.PORT,function(){
+//   console.log("Started on PORT 5000");
+// })
+
+server.listen(5000, () => {console.log("Started on PORT 5000");})
+
