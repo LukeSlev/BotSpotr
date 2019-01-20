4ea0c3f0d164b3d9271c65c9069ad3d9ae75fcf0
LukeSlev <lslevins@ualberta.ca>
2019-01-19 23:49:43 -0700

    Wow we got some tweeter ids comin in hot baby

--- server/requests/TwitterCheck.js
+++ server/requests/TwitterCheck.js
@@ -2,60 +2,64 @@ const Twitter = require('twitter');
 const request = require('request-promise');
 require('dotenv').config();
 
-var tok;
+
+
+function auth(){
+  let consKey = process.env.TWITTER_CONSUMER_KEY;
+  let consSecret = process.env.TWITTER_CONSUMER_SECRET_KEY;
+
+  let rfc = Buffer.from(`${consKey}:${consSecret}`).toString('base64');
+
+  var options = {
+    method: 'POST',
+    url: "https://api.twitter.com/oauth2/token",
+    headers: {
+      "Authorization":"Basic " + rfc,
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: "grant_type=client_credentials",
+  }
+
+  return request(options)
+        .then((parseResp) => {
+            //do something
+            return JSON.parse(parseResp).access_token;
+        })
+        .catch(function(err) {
+            console.log("OAuth Twitter POST failed "+ err);
+        });
+}
+
 
 module.exports = {
+  getRetweeters : async function(payload){
+    var tok = await auth();
 
-  auth : function (){
-    let consKey = process.env.TWITTER_CONSUMER_KEY;
-    let consSecret = process.env.TWITTER_CONSUMER_SECRET_KEY;
-
-    let rfc = Buffer.from(`${consKey}:${consSecret}`).toString('base64');
+    var client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET_KEY,
+      bearer_token: tok,
+    });
 
     var options = {
-      method: 'POST',
-      url: "https://api.twitter.com/oauth2/token",
-      headers: {
-        "Authorization":"Basic " + rfc,
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-      body: "grant_type=client_credentials",
+      count: 10,
+      id: payload,
+      stringify_ids: true,
     }
-
-    request(options)
-          .then((parseResp) => {
-              //do something
-              tok =  JSON.parse(parseResp).access_token;
-          })
-          .catch(function(err) {
-              console.log("OAuth Twitter POST failed "+ err);
-          });
+    client.get('statuses/retweeters/ids.json', options , function(error, tweets, response) {
+      if (!error) {
+        // JSON.parse(parseResp).access_token;
+        // for ()
+      }
+      tweets.ids.forEach(element => {
+        console.log(element);
+      });
+   });
   }
 }
 
-function getRetweeters(payload){
-  var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET_KEY,
-    bearer_token: tok,
-  });
+// function main() {
+//   getRetweeters("helplo");
+// }
 
-  var setBearerToken = (bt) => {
-    this.setBearerToken = bt;
-  }
-
-  Twitter.prototype.setBearerToken = setBearerToken;
-
-  client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-    if (!error) {
-      console.log("error " + error);
-    }
-    console.log(tweets);
- });
-}
-
-function main() {
-  getRetweeters("helplo");
-}
-
-main();
+// main();
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var twitter;
+var twitter = require('./requests/TwitterCheck');
 
-require('dotenv').config();
+require('dotenv').config({path:'../.env'});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,8 +12,10 @@ app.post('/search',function(req,res){
   var url=req.body.url;
   console.log("search twitter url: "+url);
   // do twitter search
-
+  var id = url.substring(url.lastIndexOf('/') + 1);
+  console.log("This id babyyyy: "+id);
+  twitter.getRetweeters(id);
 });
 app.listen(process.env.PORT,function(){
-  console.log("Started on PORT 5000");
+  console.log("Started on PORT" + process.env.PORT);
 })
